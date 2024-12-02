2574f903586fe10ff3a65c00bf1f28f7828d5747
vo1dstarr <void@vo1dstarr.com>
2024-12-02 00:53:35 -0600

    Solution to part 2

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,23 @@
-use core::num;
-use std::{collections::HashMap, fs};
+use std::fs;
 
 fn main() {
     let input = fs::read_to_string("input.txt").unwrap();
 
     let num_safe = safe_reports(&input);
     println!("Number of safe reports: {}", num_safe);
+
+    let num_damp_safe = damp_safe_reports(&input);
+    println!(
+        "Number of safe reports with Problem Dampener: {}",
+        num_damp_safe
+    );
 }
 
 fn safe_reports(input: &str) -> i32 {
+    safe_reports_with(input, is_safe)
+}
+
+fn safe_reports_with(input: &str, f: impl Fn(&[i32]) -> bool) -> i32 {
     input
         .lines()
         .map(|s| {
@@ -16,7 +25,7 @@ fn safe_reports(input: &str) -> i32 {
                 .map(|s| s.parse::<i32>().unwrap())
                 .collect::<Vec<_>>()
         })
-        .map(|v| is_safe(&v))
+        .map(|v| f(&v))
         .fold(0, |acc, report| {
             let mut result = acc;
             if report {
@@ -49,6 +58,29 @@ fn is_safe(levels: &[i32]) -> bool {
             }
         })
         .is_ok()
+}
+
+fn damp_safe_reports(input: &str) -> i32 {
+    safe_reports_with(input, is_damp_safe)
+}
+
+fn is_damp_safe(levels: &[i32]) -> bool {
+    if is_safe(levels) {
+        return true;
+    }
+    for i in 0..levels.len() {
+        let mut damp_report = Vec::with_capacity(levels.len() - 1);
+        let split = levels.split_at(i);
+        damp_report.extend_from_slice(split.0);
+        let final_slice = split.1.get(1..);
+        if let Some(slice) = final_slice {
+            damp_report.extend_from_slice(slice);
+        }
+        if is_safe(&damp_report) {
+            return true;
+        }
+    }
+    false
 }
 
 #[cfg(test)]
@@ -107,6 +139,60 @@ mod tests {
     fn test_is_safe6() {
         let input = [1, 3, 6, 7, 9];
         let result = is_safe(&input);
+        assert!(result);
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = "7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9";
+        let result = damp_safe_reports(input);
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn test_is_damp_safe1() {
+        let input = [7, 6, 4, 2, 1];
+        let result = is_damp_safe(&input);
+        assert!(result);
+    }
+
+    #[test]
+    fn test_is_damp_safe2() {
+        let input = [1, 2, 7, 8, 9];
+        let result = is_damp_safe(&input);
+        assert!(!result);
+    }
+
+    #[test]
+    fn test_is_damp_safe3() {
+        let input = [9, 7, 6, 2, 1];
+        let result = is_damp_safe(&input);
+        assert!(!result);
+    }
+
+    #[test]
+    fn test_is_damp_safe4() {
+        let input = [1, 3, 2, 4, 5];
+        let result = is_damp_safe(&input);
+        assert!(result);
+    }
+
+    #[test]
+    fn test_is_damp_safe5() {
+        let input = [8, 6, 4, 4, 1];
+        let result = is_damp_safe(&input);
+        assert!(result);
+    }
+
+    #[test]
+    fn test_is_damp_safe6() {
+        let input = [1, 3, 6, 7, 9];
+        let result = is_damp_safe(&input);
         assert!(result);
     }
 }

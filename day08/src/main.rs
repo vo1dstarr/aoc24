6dc8ced11dba84dc08f15caef8460dac5ed14ede
vo1dstarr <void@vo1dstarr.com>
2024-12-08 01:25:18 -0600

    solved day 8 part 2

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -3,12 +3,21 @@ use std::{collections::HashMap, fs};
 fn main() {
     let input = fs::read_to_string("input.txt").unwrap();
     println!("Answer to part1: {}", part1(&input));
-    // println!("Answer to part2: {}", part2(&input));
+    println!("Answer to part2: {}", part2(&input));
 }
 
 type Matrix<T> = Vec<Vec<T>>;
 
 fn part1(input: &str) -> usize {
+    let (ant_matrix, mut node_matrix) = parse(input);
+    let ant_map = map_ants(&ant_matrix);
+    for antenas in ant_map.values() {
+        generate_nodes(&mut node_matrix, antenas, insert_nodes_in_direction);
+    }
+    count_nodes(&node_matrix)
+}
+
+fn parse(input: &str) -> (Vec<Vec<AntSpace>>, Vec<Vec<NodeSpace>>) {
     let mut ant_matrix = Vec::new();
     let mut node_matrix = Vec::new();
     for line in input.lines() {
@@ -24,14 +33,15 @@ fn part1(input: &str) -> usize {
         ant_matrix.push(ant_row);
         node_matrix.push(node_row);
     }
-    // dbg!(&ant_matrix);
+    (ant_matrix, node_matrix)
+}
 
+fn part2(input: &str) -> usize {
+    let (ant_matrix, mut node_matrix) = parse(input);
     let ant_map = map_ants(&ant_matrix);
     for antenas in ant_map.values() {
-        generate_nodes(&mut node_matrix, antenas);
+        generate_nodes(&mut node_matrix, antenas, insert_res_nodes_in_direction);
     }
-
-    // dbg!(&node_matrix);
     count_nodes(&node_matrix)
 }
 
@@ -49,7 +59,9 @@ fn map_ants(matrix: &Matrix<AntSpace>) -> HashMap<char, Vec<(usize, usize)>> {
     map
 }
 
-fn generate_nodes(matrix: &mut Matrix<NodeSpace>, antenas: &[(usize, usize)]) {
+type InsertFn = fn(&mut Matrix<NodeSpace>, isize, isize, isize, isize);
+
+fn generate_nodes(matrix: &mut Matrix<NodeSpace>, antenas: &[(usize, usize)], insert: InsertFn) {
     if antenas.len() < 2 {
         return;
     }
@@ -63,8 +75,8 @@ fn generate_nodes(matrix: &mut Matrix<NodeSpace>, antenas: &[(usize, usize)]) {
             let ant2x = ant2.1 as isize;
             let diffy = ant1y - ant2y;
             let diffx = ant1x - ant2x;
-            insert_nodes_in_direction(matrix, ant1y, ant1x, diffy, diffx);
-            insert_nodes_in_direction(matrix, ant2y, ant2x, -diffy, -diffx);
+            insert(matrix, ant1y, ant1x, diffy, diffx);
+            insert(matrix, ant2y, ant2x, -diffy, -diffx);
         }
     }
 }
@@ -79,17 +91,32 @@ fn insert_nodes_in_direction(
     let m_len = matrix.len();
     let m_wid = matrix[0].len();
 
-    let mut y = starty;
-    let mut x = startx;
-    // loop {
-    y += offsety;
-    x += offsetx;
+    let y = starty + offsety;
+    let x = startx + offsetx;
     if y < 0 || x < 0 || y >= m_len as isize || x >= m_wid as isize {
         return;
     }
-    // println!("Gen node at {},{}", y, x);
     matrix[y as usize][x as usize] = NodeSpace::Node;
-    // }
+}
+
+fn insert_res_nodes_in_direction(
+    matrix: &mut Matrix<NodeSpace>,
+    starty: isize,
+    startx: isize,
+    offsety: isize,
+    offsetx: isize,
+) {
+    let m_len = matrix.len();
+    let m_wid = matrix[0].len();
+
+    let mut y = starty;
+    let mut x = startx;
+
+    while !(y < 0 || x < 0 || y >= m_len as isize || x >= m_wid as isize) {
+        matrix[y as usize][x as usize] = NodeSpace::Node;
+        y += offsety;
+        x += offsetx;
+    }
 }
 
 fn count_nodes(matrix: &Matrix<NodeSpace>) -> usize {
@@ -139,5 +166,23 @@ mod tests {
 ............";
         let answer = part1(input);
         assert_eq!(answer, 14);
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = "............
+........0...
+.....0......
+.......0....
+....0.......
+......A.....
+............
+............
+........A...
+.........A..
+............
+............";
+        let answer = part2(input);
+        assert_eq!(answer, 34);
     }
 }

a4ce37168680668e4f8964d11f038b193f8ad77b
vo1dstarr <void@vo1dstarr.com>
2024-12-07 19:01:51 -0600

    solved day 7 part 1

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -1,12 +1,56 @@
-use std::fs;
+use std::{collections::VecDeque, fs};
 
 fn main() {
     let input = fs::read_to_string("input.txt").unwrap();
     println!("Answer to part1: {}", part1(&input));
 }
 
-fn part1(input: &str) -> i32 {
-    todo!()
+fn part1(input: &str) -> i64 {
+    input
+        .lines()
+        .map(|line| {
+            let mut split_iter = line.split(": ");
+            let test: i64 = split_iter.next().unwrap().parse().unwrap();
+            let nums: Vec<i64> = split_iter
+                .next()
+                .unwrap()
+                .split(' ')
+                .map(|s| s.parse().unwrap())
+                .collect();
+            (test, nums)
+        })
+        .flat_map(|(test, nums)| {
+            if valid_ops_exist(test, &nums) {
+                Some(test)
+            } else {
+                None
+            }
+        })
+        .sum()
+}
+
+fn valid_ops_exist(test: i64, nums: &[i64]) -> bool {
+    let mut queue = VecDeque::new();
+    queue.push_back(nums[0]);
+    for rhs in &nums[1..] {
+        let mut new_queue = VecDeque::new();
+        while let Some(lhs) = queue.pop_front() {
+            let new_nums = [lhs + rhs, lhs * rhs];
+            for num in new_nums {
+                // if num is greater than test, then we can prune this branch from the search tree
+                if num <= test {
+                    new_queue.push_back(num);
+                }
+            }
+        }
+        queue = new_queue;
+    }
+    for n in queue {
+        if n == test {
+            return true;
+        }
+    }
+    false
 }
 
 #[cfg(test)]
